c893c52472b2e272df0ee163219f84208df2c3a7
KO TAEWON <migmaknoob@gmail.com>
2025-02-25 17:46:02 +0900

    PS

--- PS_BOJ/src/num1932.java
+++ PS_BOJ/src/num1932.java
@@ -6,25 +6,29 @@ public class num1932 {
 
         int n = sc.nextInt();
 
-        int[][] arr = new int[n][n];
-        int[][] dp = new int[n][n];
+        int[][] arr = new int[n+1][n+1];
+        int[][] dp = new int[n+1][n+1];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i+1; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j < i+1; j++) {
                 arr[i][j] = sc.nextInt();
             }
 
         }
 
-        Print2D.print2DArray(arr);
 
-        dp[0][0] = arr[0][0];
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i+1; j++) {
-                dp[i][j] = dp[i-1][j] + arr[i-1][j];
+        dp[1][1] = arr[1][1];
+        for (int i = 1; i <= n; i++) { // 1일때
+            for (int j = 1; j < i+1; j++) { // 0부터 2까지 1, 00, = + i + j 1, 0
+                dp[i][j] = Math.max(dp[i-1][j-1] +arr[i][j], dp[i-1][j] + arr[i][j]);
             }
         }
 
-        Print2D.print2DArray(dp);
+        int a = 0;
+        for (int i = 1; i <= n; i++) {
+            a = Math.max(a, dp[n][i]);
+        }
+
+        System.out.println(a);
     }
 }
---
+++ PS_BOJ/src/num2156.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class num2156 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //포도주 잔을 선택하면 그 잔에 들어있는 포도주는 모두 마셔야 하고, 마신 후에는 원래 위치에 다시 놓아야 한다.
+        //연속으로 놓여 있는 3잔을 모두 마실 수는 없다.
+
+        // 3잔만 마실수 있는게 아님, 마실 수 있는 와인의 최대 수는 없음
+        // 3잔 연속 마시는것만 아니면 됨.
+    }
+}
