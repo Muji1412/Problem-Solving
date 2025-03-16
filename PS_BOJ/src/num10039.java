d6951eab5a0e1ac213b93c2237d0328c6bf83a62
TAEWON KO <migmaknoob@gmail.com>
2025-03-16 21:46:49 +0900

    PS

---
+++ PS_BOJ/src/num10039.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class num10039 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int a=sc.nextInt();
+            if (a < 40) {
+                sum += 40;
+            } else {
+                sum += a;
+            }
+        }
+
+        System.out.println(sum / 5);
+    }
+}
